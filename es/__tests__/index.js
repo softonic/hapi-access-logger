218cead0bacf141a2a4c7f3386d4d0431ad60513
Rubén Norte <ruben.norte@softonic.com>
2017-03-14 16:54:49 +0100

    First version

---
+++ es/__tests__/index.js
@@ -0,0 +1,161 @@
+import hapi from 'hapi';
+import HapiAccessLogs from '../index';
+
+function createServerWithPlugin(options) {
+  const server = new hapi.Server();
+  server.connection();
+  server.register({
+    register: HapiAccessLogs,
+    options,
+  });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => (
+      reply('Hola mundo')
+        .header('x-bar', 'baz')
+        .header('content-type', 'text/plain; charset=utf-8')
+        .header('content-language', 'es-ES')
+    ),
+  });
+  return { server };
+}
+
+describe('HapiAccessLogs', () => {
+  it('should be a Hapi plugin', () => {
+    expect(HapiAccessLogs.register).toBeInstanceOf(Function);
+    expect(HapiAccessLogs.register.attributes.pkg.name).toBe('@softonic/hapi-access-logger');
+  });
+
+  describe('when it is registered', () => {
+    it('should log a completed request', async () => {
+      const logger = {
+        info: jest.fn(),
+      };
+      const { server } = createServerWithPlugin({ logger });
+
+      await server.inject({
+        method: 'GET',
+        url: '/',
+        headers: {
+          'x-foo': 'bar',
+        },
+      });
+
+      expect(logger.info).toHaveBeenCalledWith({
+        request: expect.objectContaining({
+          method: 'GET',
+          url: '/',
+          headers: expect.objectContaining({
+            'x-foo': 'bar',
+          }),
+        }),
+        response: expect.objectContaining({
+          statusCode: 200,
+          timestamp: expect.any(String),
+          headers: expect.objectContaining({
+            'x-bar': 'baz',
+          }),
+        }),
+      });
+    });
+
+    it('should whitelist the specified headers', async () => {
+      const logger = {
+        info: jest.fn(),
+      };
+      const { server } = createServerWithPlugin({
+        logger,
+        whitelistHeaders: [
+          'host',
+          'accept',
+          'accept-language',
+          'content-type',
+          'content-language',
+        ],
+      });
+
+      await server.inject({
+        method: 'GET',
+        url: '/',
+        headers: {
+          'x-foo': 'bar',
+          host: 'example.com',
+          accept: 'text/plain',
+          'accept-language': 'es-ES',
+        },
+      });
+
+      expect(logger.info).toHaveBeenCalledWith({
+        request: expect.objectContaining({
+          method: 'GET',
+          url: '/',
+          headers: {
+            host: 'example.com',
+            accept: 'text/plain',
+            'accept-language': 'es-ES',
+          },
+        }),
+        response: expect.objectContaining({
+          statusCode: 200,
+          timestamp: expect.any(String),
+          headers: {
+            'content-type': 'text/plain; charset=utf-8',
+            'content-language': 'es-ES',
+          },
+        }),
+      });
+    });
+
+    it('should blacklist the specified headers', async () => {
+      const logger = {
+        info: jest.fn(),
+      };
+      const { server } = createServerWithPlugin({
+        logger,
+        blacklistHeaders: [
+          'accept',
+          'accept-language',
+          'content-type',
+          'content-language',
+        ],
+      });
+
+      await server.inject({
+        method: 'GET',
+        url: '/',
+        headers: {
+          'x-foo': 'bar',
+          host: 'example.com',
+          accept: 'text/plain',
+          'accept-language': 'es-ES',
+        },
+      });
+
+      expect(logger.info).toHaveBeenCalledWith({
+        request: expect.objectContaining({
+          method: 'GET',
+          url: '/',
+          headers: expect.objectContaining({
+            'x-foo': 'bar',
+            host: 'example.com',
+          }),
+        }),
+        response: expect.objectContaining({
+          statusCode: 200,
+          timestamp: expect.any(String),
+          headers: expect.objectContaining({
+            'x-bar': 'baz',
+          }),
+        }),
+      });
+
+      const logEntry = logger.info.mock.calls[0][0];
+
+      expect(logEntry.request.headers.accept).toBeUndefined();
+      expect(logEntry.request.headers['accept-language']).toBeUndefined();
+      expect(logEntry.response.headers['content-type']).toBeUndefined();
+      expect(logEntry.response.headers['content-language']).toBeUndefined();
+    });
+  });
+});
---
+++ es/index.js
@@ -0,0 +1,89 @@
+import { formatRequest, formatResponse } from '@softonic/http-log-format';
+import { pick, omit } from 'lodash';
+import packageJSON from '../package.json';
+
+/**
+ * Filters the given headers object picking the given whitelisted headers (if any) and removing
+ * all blacklisted ones
+ * @param  {Object.<string, string>} options.headers
+ * @param  {string[]} [options.whitelistHeaders]
+ * @param  {string[]} [options.blacklistHeaders]
+ * @return {Object.<string, string>}
+ */
+function filterHeaders({ headers, whitelistHeaders, blacklistHeaders }) {
+  const whitelistedHeaders = whitelistHeaders ? pick(headers, whitelistHeaders) : headers;
+  return omit(whitelistedHeaders, blacklistHeaders);
+}
+
+/**
+ * Hapi plugin to log finished requests and responses
+ *
+ * @example
+ *
+ * // Registration
+ * server.register({
+ *   register: HapiAccessLogs,
+ *   options: {
+ *     logger: bunyan.createLogger({ name: 'access-log' })
+ *   }
+ * }, (error) => {});
+ *
+ * @type {Object}
+ */
+const HapiAccessLogs = {
+
+  /**
+   * Registers the plugin in the Hapi server
+   * @param  {hapi.Server}  server
+   * @param  {Object}       options
+   * @param  {Logger}       options.logger
+   * @param  {Function}     notifyRegistration
+   */
+  register(server, options, notifyRegistration) {
+    const { logger, whitelistHeaders, blacklistHeaders } = options;
+
+    server.on('response', (request) => {
+      const { req, res } = request.raw;
+
+      const receivedTime = new Date(request.info.received);
+      const sentTime = new Date();
+
+      const extendedReq = Object.assign({}, req, {
+        timestamp: receivedTime.toISOString(),
+      });
+
+      const extendedRes = Object.assign({}, res, {
+        timestamp: sentTime.toISOString(),
+        responseTime: sentTime - receivedTime,
+      });
+
+      const loggableRequest = formatRequest(extendedReq);
+      loggableRequest.headers = filterHeaders({
+        headers: loggableRequest.headers,
+        whitelistHeaders,
+        blacklistHeaders,
+      });
+
+      const loggableResponse = formatResponse(extendedRes);
+      loggableResponse.headers = filterHeaders({
+        headers: loggableResponse.headers,
+        whitelistHeaders,
+        blacklistHeaders,
+      });
+
+      logger.info({
+        request: loggableRequest,
+        response: loggableResponse,
+      });
+    });
+
+    notifyRegistration();
+  },
+
+};
+
+HapiAccessLogs.register.attributes = {
+  pkg: packageJSON,
+};
+
+export default HapiAccessLogs;
